542398f5fdc7238dbdeaee7a10624cda9289a499
hany73-max <hanyelashry323@gmail.com>
2026-07-15 22:25:37 +0300

    adding the https to fix the api url

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def run_pipeline(df_raw: pd.DataFrame):
 
 import os
 
-API_URL = os.environ.get("API_URL", "kessler-shield-production.up.railway.app")
+API_URL = os.environ.get("API_URL", "https://kessler-shield-production.up.railway.app/")
 
 
 def predict_via_api(row: dict) -> float:
